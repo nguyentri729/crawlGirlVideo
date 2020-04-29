f36b4b3b8b2adfd199b45585b72d4ab1953aaeb1
Nguyen Tri <nguyentri729.2018@gmail.com>
2020-04-29 22:39:42 +0700

    fix cors

--- controllers/girl.controller.js
+++ controllers/girl.controller.js
@@ -1,17 +1,35 @@
 const { getVideo } = require("../modules/getVideo");
+const fs = require('fs')
+const {shuffleArray} = require('../helpers/index')
 const girlController = async (req, res) => {
-  const limit = req.query.limit ? req.query.limit : 25;
+  
+  const limit = req.query.limit ? req.query.limit : 100;
   const source = req.query.source ? req.query.source : "facebook";
-  const sort = req.query.limit ? req.query.limit : "default";
+  const sort = req.query.sort ? req.query.sort : "default";
+  const type = req.query.type ? req.query.type : "local";
+  
+  if (type == 'local') {
+    fs.readFile('./localVideo.json', 'utf8', function(err, data){
+      res.json(shuffleArray(JSON.parse(data)))
+    })
+  }else{
+    const pageVideo = [
+      'https://www.facebook.com/ohmyladyvn/videos/',
+      'https://www.facebook.com/girlsfromasiatoeurope/videos/',
+      'https://www.facebook.com/pg/VSBGPage/videos/'
+    ]
+    const fetchVideo = new getVideo(pageVideo[Math.round(Math.random() * pageVideo.length)]);
+    await fetchVideo.chormeInit({
+      headless: true,
+    });
+    const data = await fetchVideo.getVideo(limit, source, sort);
+    fs.writeFile('./localVideo.json', JSON.stringify(data), function(data) {
+      console.log(data)
+    })
+    res.json(data);
 
-  const fetchVideo = new getVideo(
-    "https://www.facebook.com/ohmyladyvn/videos/"
-  );
-  await fetchVideo.chormeInit({
-    headless: true,
-  });
-  const data = await fetchVideo.getVideo(limit, source, sort);
-  res.json(data);
+  }
+
 };
 module.exports = {
   girlController,
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const girlRoute = require('./routes/girl.route')
+const cors = require('cors')
+
+app.use(cors())
 
 app.use('/girl', girlRoute)
 
-app.listen(3000);
+app.listen(6969);
+
+
